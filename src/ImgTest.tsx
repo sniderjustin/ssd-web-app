d7e21ee404d9dcfc33b5e89f49b73dc6466ae268
Justin Snider <js10853@nyu.edu>
2023-01-17 07:45:56 -0800

    add detect example to project

--- src/ImgTest.tsx
+++ src/ImgTest.tsx
@@ -4,31 +4,50 @@ import * as cocoSsd from '@tensorflow-models/coco-ssd';
 import './App.css';
 
 
-async function onFirstLoad() {
-    try {
-        const model = await cocoSsd.load();
-        // setModel(model);
-        console.log("setloadedModel");
-        console.log(model);
-      } catch (err) {
-        console.log(err);
-        console.log("failed load model");
-      }
 
-};
 
 function ImgTest() {
+
+    const [model, setModel] = useState();
+
+    async function loadModel() {
+        try {
+            const model = await cocoSsd.load();
+            setModel(model);
+            console.log("setloadedModel");
+            console.log(model);
+          } catch (err) {
+            console.log(err);
+            console.log("failed load model");
+          }
+    
+    };
+
+
   useEffect(() => {
     tf.ready().then(() => {
-        onFirstLoad();
+        loadModel();
       });
   }, []);
 
+ async function predictFunction() {
+    const images = document.getElementById("myimage");
+    console.log(images)
+    const predictions = await model.detect(images);
+    console.log('predictions');
+    console.log(predictions);
+ }
+
   return (
     <div className='classifyOnClick'>
-    <img src='https://cdn.glitch.com/5bf7c54b-c36f-4009-a191-186909fb788e%2Fdog_flickr_publicdomain.jpg?v=1579209396930' 
-      width={400}
+    <img id='myimage'
+        crossOrigin='anonymous'
+        src='https://cdn.glitch.com/5bf7c54b-c36f-4009-a191-186909fb788e%2Fdog_flickr_publicdomain.jpg?v=1579209396930' 
+        width={400}
     />
+    <button onClick={predictFunction}>
+    Start Detect
+    </button>
   </div>
   );
 }
